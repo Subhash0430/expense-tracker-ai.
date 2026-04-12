0688dde1622794d18fa42f77d8f1d0017a73d005
Subhash R <subhashsanju2003@gmail.com>
2026-04-12 11:38:00 +0530

    Auto create superuser on deploy if env vars are present

--- build.sh
+++ build.sh
@@ -3,3 +3,8 @@
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+# Automatically create a superuser if environment variables are provided
+if [[ -n "${DJANGO_SUPERUSER_USERNAME}" && -n "${DJANGO_SUPERUSER_PASSWORD}" && -n "${DJANGO_SUPERUSER_EMAIL}" ]]; then
+    python manage.py createsuperuser --noinput || true
+fi

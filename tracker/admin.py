ba039fb0a8176081ae49216f4063336929ab26e0
Subhash R <subhashsanju2003@gmail.com>
2026-04-12 11:24:54 +0530

    Register models to admin interface

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Expense, Budget
 
-# Register your models here.
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'amount', 'category', 'date')
+    list_filter = ('category', 'date', 'user')
+    search_fields = ('title', 'category', 'note')
+
+@admin.register(Budget)
+class BudgetAdmin(admin.ModelAdmin):
+    list_display = ('user', 'category', 'monthly_limit', 'month', 'year')
+    list_filter = ('category', 'month', 'year')
+    search_fields = ('user__username', 'category')
